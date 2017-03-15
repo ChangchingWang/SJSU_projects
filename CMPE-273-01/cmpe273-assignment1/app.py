25d4b43e2217e3309eb9b354eb951d24cd884c9c
CHANG-CHING WANG <chinchin0207@gmail.com>
2017-03-14 22:56:26 -0700

    Complete Assignment1
    complete assignment1

--- CMPE-273-01/cmpe273-assignment1/app.py
+++ CMPE-273-01/cmpe273-assignment1/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import sys
+import requests
+
+#https://github.com/sithu/assignment1-config-example
+sysArg1 = sys.argv[1]
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello from Dockerized Flask App!!"
+@app.route("/v1/<fileName>")
+def hello(fileName):
+	global sysArg1
+	urlStr = sysArg1.replace("https://", "")
+	fields = urlStr.split("/")
+	owner = fields[1]
+	repo = fields[2]
+	url="https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + fileName
+	headers = {'Accept' : 'application/vnd.github.v3.raw'}
+	r = requests.get(url, headers=headers)
+	return r.text;
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0')
